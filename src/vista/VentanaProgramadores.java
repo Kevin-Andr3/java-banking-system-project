2a97fd4c4add4830bb2f16a9a2d53ec467df5f1d
Kevin-Andr3 <kevin.delacruz.vi@gmail.com>
2026-07-14 19:15:11 -0500

    initial clean commit of java-banking-system

--- src/vista/VentanaProgramadores.java
+++ src/vista/VentanaProgramadores.java
@@ -21,19 +21,14 @@ public class VentanaProgramadores extends JDialog implements ActionListener {
 	private JLabel lblFoto5;
 	private JLabel lblBasilioAlvarez;
 	private JLabel lblGianellaAnnie;
-	private JLabel lblN00396701;
 	private JLabel lblCahuanaMoquillaza;
 	private JLabel lblHerbertJherson;
-	private JLabel lblN00327161;
 	private JLabel lblCorcueraParedes;
 	private JLabel lblJoseManuel;
-	private JLabel lblN00373084;
 	private JLabel lblCotrinaSandoval;
 	private JLabel lblAlexandraMargoth;
-	private JLabel lblN00328935;
-	private JLabel lblDeLaCruzVilluga;
+	private JLabel lblDeLaCruzVillugas;
 	private JLabel lblKevinAndre;
-	private JLabel lblN00368206;
 	private JLabel lblFoto1;
 	
 	public VentanaProgramadores() {
@@ -61,7 +56,7 @@ public class VentanaProgramadores extends JDialog implements ActionListener {
 		}
 		{
 			lblFoto2 = new JLabel("");
-			lblFoto2.setIcon(new ImageIcon(VentanaProgramadores.class.getResource("/imagen/FotoCarnet_CahuanaMoquillaza.jpeg")));
+			lblFoto2.setIcon(new ImageIcon(VentanaProgramadores.class.getResource("/imagen/avatar.jpg")));
 			lblFoto2.setHorizontalAlignment(SwingConstants.CENTER);
 			lblFoto2.setForeground(new Color(0, 0, 0));
 			lblFoto2.setBackground(new Color(255, 255, 255));
@@ -70,7 +65,7 @@ public class VentanaProgramadores extends JDialog implements ActionListener {
 		}
 		{
 			lblFoto3 = new JLabel("");
-			lblFoto3.setIcon(new ImageIcon(VentanaProgramadores.class.getResource("/imagen/fotoCarnet_CorcueraParedes.jpeg")));
+			lblFoto3.setIcon(new ImageIcon(VentanaProgramadores.class.getResource("/imagen/avatar.jpg")));
 			lblFoto3.setForeground(new Color(0, 0, 0));
 			lblFoto3.setBackground(new Color(255, 255, 255));
 			lblFoto3.setBounds(370, 120, 137, 171);
@@ -78,7 +73,7 @@ public class VentanaProgramadores extends JDialog implements ActionListener {
 		}
 		{
 			lblFoto4 = new JLabel("");
-			lblFoto4.setIcon(new ImageIcon(VentanaProgramadores.class.getResource("/imagen/fotoCarnet_CotrinaSandoval.jpeg")));
+			lblFoto4.setIcon(new ImageIcon(VentanaProgramadores.class.getResource("/imagen/avatar.jpg")));
 			lblFoto4.setForeground(new Color(0, 0, 0));
 			lblFoto4.setBackground(new Color(255, 255, 255));
 			lblFoto4.setBounds(530, 120, 137, 171);
@@ -87,7 +82,7 @@ public class VentanaProgramadores extends JDialog implements ActionListener {
 		{
 			lblFoto5 = new JLabel("");
 			lblFoto5.setHorizontalAlignment(SwingConstants.CENTER);
-			lblFoto5.setIcon(new ImageIcon(VentanaProgramadores.class.getResource("/imagen/FotoCarnet_DeLaCruzVillugas.jpeg")));
+			lblFoto5.setIcon(new ImageIcon(VentanaProgramadores.class.getResource("/imagen/avatar.jpg")));
 			lblFoto5.setForeground(new Color(0, 0, 0));
 			lblFoto5.setBackground(new Color(255, 255, 255));
 			lblFoto5.setBounds(690, 120, 137, 171);
@@ -110,14 +105,6 @@ public class VentanaProgramadores extends JDialog implements ActionListener {
 			getContentPane().add(lblGianellaAnnie);
 		}
 		{
-			lblN00396701 = new JLabel("(N00396701)");
-			lblN00396701.setHorizontalAlignment(SwingConstants.CENTER);
-			lblN00396701.setForeground(new Color(90, 90, 90));
-			lblN00396701.setFont(new Font("Arial", Font.BOLD, 15));
-			lblN00396701.setBounds(50, 368, 137, 18);
-			getContentPane().add(lblN00396701);
-		}
-		{
 			lblCahuanaMoquillaza = new JLabel("Cahuana Moquillaza");
 			lblCahuanaMoquillaza.setHorizontalAlignment(SwingConstants.CENTER);
 			lblCahuanaMoquillaza.setForeground(new Color(90, 90, 90));
@@ -132,14 +119,6 @@ public class VentanaProgramadores extends JDialog implements ActionListener {
 			lblHerbertJherson.setFont(new Font("Arial", Font.BOLD, 15));
 			lblHerbertJherson.setBounds(210, 339, 137, 18);
 			getContentPane().add(lblHerbertJherson);
-		}
-		{
-			lblN00327161 = new JLabel("(N00327161)");
-			lblN00327161.setHorizontalAlignment(SwingConstants.CENTER);
-			lblN00327161.setForeground(new Color(90, 90, 90));
-			lblN00327161.setFont(new Font("Arial", Font.BOLD, 15));
-			lblN00327161.setBounds(210, 368, 137, 18);
-			getContentPane().add(lblN00327161);
 		}
 		{
 			lblCorcueraParedes = new JLabel("Corcuera Paredes");
@@ -158,14 +137,6 @@ public class VentanaProgramadores extends JDialog implements ActionListener {
 			getContentPane().add(lblJoseManuel);
 		}
 		{
-			lblN00373084 = new JLabel("(N00373084)");
-			lblN00373084.setHorizontalAlignment(SwingConstants.CENTER);
-			lblN00373084.setForeground(new Color(90, 90, 90));
-			lblN00373084.setFont(new Font("Arial", Font.BOLD, 15));
-			lblN00373084.setBounds(370, 368, 137, 18);
-			getContentPane().add(lblN00373084);
-		}
-		{
 			lblCotrinaSandoval = new JLabel("Cotrina Sandoval");
 			lblCotrinaSandoval.setHorizontalAlignment(SwingConstants.CENTER);
 			lblCotrinaSandoval.setForeground(new Color(90, 90, 90));
@@ -182,20 +153,12 @@ public class VentanaProgramadores extends JDialog implements ActionListener {
 			getContentPane().add(lblAlexandraMargoth);
 		}
 		{
-			lblN00328935 = new JLabel("(N00328935)");
-			lblN00328935.setHorizontalAlignment(SwingConstants.CENTER);
-			lblN00328935.setForeground(new Color(90, 90, 90));
-			lblN00328935.setFont(new Font("Arial", Font.BOLD, 15));
-			lblN00328935.setBounds(530, 368, 137, 18);
-			getContentPane().add(lblN00328935);
-		}
-		{
-			lblDeLaCruzVilluga = new JLabel("De La Cruz Villuga");
-			lblDeLaCruzVilluga.setHorizontalAlignment(SwingConstants.CENTER);
-			lblDeLaCruzVilluga.setForeground(new Color(90, 90, 90));
-			lblDeLaCruzVilluga.setFont(new Font("Arial", Font.BOLD, 15));
-			lblDeLaCruzVilluga.setBounds(690, 310, 137, 18);
-			getContentPane().add(lblDeLaCruzVilluga);
+			lblDeLaCruzVillugas = new JLabel("De La Cruz Villugas");
+			lblDeLaCruzVillugas.setHorizontalAlignment(SwingConstants.CENTER);
+			lblDeLaCruzVillugas.setForeground(new Color(90, 90, 90));
+			lblDeLaCruzVillugas.setFont(new Font("Arial", Font.BOLD, 15));
+			lblDeLaCruzVillugas.setBounds(690, 310, 137, 18);
+			getContentPane().add(lblDeLaCruzVillugas);
 		}
 		{
 			lblKevinAndre = new JLabel("Kevin André");
@@ -206,16 +169,8 @@ public class VentanaProgramadores extends JDialog implements ActionListener {
 			getContentPane().add(lblKevinAndre);
 		}
 		{
-			lblN00368206 = new JLabel("(N00368206)");
-			lblN00368206.setHorizontalAlignment(SwingConstants.CENTER);
-			lblN00368206.setForeground(new Color(90, 90, 90));
-			lblN00368206.setFont(new Font("Arial", Font.BOLD, 15));
-			lblN00368206.setBounds(690, 368, 137, 18);
-			getContentPane().add(lblN00368206);
-		}
-		{
 			lblFoto1 = new JLabel("");
-			lblFoto1.setIcon(new ImageIcon(VentanaProgramadores.class.getResource("/imagen/fotoCarnet_BasilioAlvarez.jpeg")));
+			lblFoto1.setIcon(new ImageIcon(VentanaProgramadores.class.getResource("/imagen/avatar.jpg")));
 			lblFoto1.setHorizontalAlignment(SwingConstants.CENTER);
 			lblFoto1.setForeground(Color.BLACK);
 			lblFoto1.setBackground(Color.WHITE);
